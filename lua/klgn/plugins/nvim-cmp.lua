e0f54d094a33bfb8ae18eeea46e773857891a33a
Kristian L. G. Nielsen <kristian@lgnielsen.dk>
2023-11-17 14:26:09 +0100

    Add some dependencies

--- lua/klgn/plugins/nvim-cmp.lua
+++ lua/klgn/plugins/nvim-cmp.lua
@@ -4,6 +4,8 @@ return {
   dependencies = {
     "hrsh7th/cmp-buffer", -- source for text in buffer
     "hrsh7th/cmp-path", -- source for file system paths
+    "hrsh7th/cmp-vsnip", -- source for file system paths
+    "hrsh7th/vim-vsnip", -- source for file system paths
     "L3MON4D3/LuaSnip", -- snippet engine
     "saadparwaiz1/cmp_luasnip", -- for autocompletion
     "rafamadriz/friendly-snippets", -- useful snippets
